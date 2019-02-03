e05ac09b4cb3d4e8185dd7162fb09a74101801fc
nadavgree <nadavgreen@pursuit.org>
2019-02-03 15:44:10 -0500

    create display layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Row, Col } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <Container>
+          <Row>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+          </Row>
+          <Row>
+          <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+            <Col>
+              <Button color="primary">primary</Button>{' '}
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
   }
 }
