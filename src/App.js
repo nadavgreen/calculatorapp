2e2f7bf1e9a2a52794639dbe912fa8941387157d
nadavgree <nadavgreen@pursuit.org>
2019-02-10 03:13:00 -0500

    made changes to structure of state and added base functionality

--- src/App.js
+++ src/App.js
@@ -12,28 +12,44 @@ const ac = () => {
 }
 
 const reverseSign = (num) => { 
-  return parseFloat(num) * -1
+  return `${parseFloat(num) * -1}`
 }
 
 const percent = (num) => {
-  return parseFloat(num) / 100
+  return `${parseFloat(num) / 100}`
+}
+
+const equals = (display, previous, operation) => {
+  if (operation === '/' && parseFloat(display) === 0) {
+    return 0
+  } else if (operation === '/' && parseFloat(display) === 0) {
+    return parseFloat(previous) / parseFloat(display)
+  } else if (operation === '+') {
+    return parseFloat(previous) + parseFloat(display)
+  } else if (operation === '-') {
+    return parseFloat(previous) - parseFloat(display)
+  } else if (operation === '*') {
+    return parseFloat(previous) * parseFloat(display)
+  }
 }
 
 const addDecimal = (string) => {
   if (!string.split('').includes('.')){
     return string+'.'
   }
+  
   return string
+
 }
 
 // Helper Function
 
-// const removeZero = (string) => {
-//   if (string.split('')[0]==='0' && string.split(1) !== '.'){
-//     return string.split('').shift(0)
-//   }
-//   return string
-// }
+const removeZero = (string) => {
+  if (string.indexOf('0') === 0 && string.indexOf('.') !== 1 ){
+    return string.substring(1,string.length)
+  } 
+  return string
+}
 
 class App extends Component {
 
@@ -44,25 +60,53 @@ class App extends Component {
       displayValue: '0',
       previousValue: null,
       operation: null,
-      waitingForNewValue: false
+      waitingForNewValue: false,
+      clear: true,
     }
 
     this.functions = {
       'AC': ac,
+      'C': ac,
       '±': reverseSign,
       '%': percent,
-      '÷': 'clear()',
-      'x': 'clear()',
-      '-': 'clear()',
-      '+': 'clear()',
-      '=': 'clear()',
+      '=': equals,
       '.': addDecimal,
     }
+
+    this.operations = {
+      '÷': '/',
+      'x': '*',
+      '-': '-',
+      '+': '+',
+    }
+
+    
   }
 
 
   handleClick = e => {
-    !this.functions[e.target.innerText] ?  this.setState({displayValue: this.state.displayValue+e.target.innerText}) : this.setState({displayValue: this.functions[e.target.innerText](this.state.displayValue)})
+    // Handle AC / C button change
+    if (!this.functions[e.target.innerText] && !this.operations[e.target.innerText]){
+      this.setState({clear:false})
+    } else if (e.target.innerText === 'C') {
+      this.setState({clear:true})
+    }
+
+    // Handle Functions and Operations
+    if (!this.functions[e.target.innerText] && !this.operations[e.target.innerText] && this.state.waitingForNewValue === true){
+      this.setState({displayValue: removeZero(e.target.innerText), waitingForNewValue: false})
+      console.log('1')
+    } else if (!this.functions[e.target.innerText] && !this.operations[e.target.innerText]){
+      this.setState({displayValue: removeZero(this.state.displayValue+e.target.innerText)})
+      console.log('2')
+    } else if (this.functions[e.target.innerText]) {
+      this.setState({displayValue: this.functions[e.target.innerText](this.state.displayValue, this.state.previousValue, this.operations[this.state.operation])})
+      console.log('3')
+    } else if (this.operations[e.target.innerText]) {
+      this.setState({operation: e.target.innerText, previousValue: this.state.displayValue, waitingForNewValue: true,})
+      console.log('4')
+    }
+    console.log('waiting', this.state.waitingForNewValue)
   }
 
   render() {
@@ -71,9 +115,14 @@ class App extends Component {
         <Container style={{'width': '50%'}}>
           <Row>
             <Displaybar displayValue={this.state.displayValue}/>
-            <Mainbody handleClick={this.handleClick}/>
+            </Row>
+            <Row>
+            <Mainbody clear={this.state.clear} handleClick={this.handleClick}/>
+            </Row>
+            <Row>
             <Lastrow handleClick={this.handleClick}/>
-          </Row>
+            </Row>
+  
         </Container>
       </>
     );
--- src/components/displaybar.js
+++ src/components/displaybar.js
@@ -5,12 +5,17 @@ const Displaybar = props => {
     const style = {
         'color':'white', 
         'backgroundColor':'black', 
-        'fontSize':'120px',
+        'fontSize':'9vw',
         'textAlign': 'right', 
-        'minHeight':'120px'
+        'minHeight':'9vw'
     }
+    // if (props.displayValue.length > 9 && props.displayValue.length < 14){
+    //     style.fontSize = `${parseInt(style.fontSize) - ((props.displayValue.length - 9)*10)}px`
+    // } else if (props.displayValue.length >= 14){
+    //     style.fontSize = '70px'
+    // }
 
-    return <Col md="12" style={{padding:'0px'}}>
+    return <Col xs="12" sm="12" md="12" style={{padding:'0px'}}>
             <Input placeholder={props.displayValue} style={style} readOnly></Input>{' '}
         </Col>
 }
